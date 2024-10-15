1ebc044665de12e93c19ed98b377620523046d6d
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-10-15 12:29:22 +0200

    Configured CORS and Sanctum

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,8 +17,8 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,localhost:5173,https://exquisite-biscuit-f0c404.netlify.app,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
     /*
